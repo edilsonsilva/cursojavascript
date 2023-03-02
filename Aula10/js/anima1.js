1251fb9c540e6a19a18b986500e464e34fcfeb0a
Edilson Silva <edilson@yahoo.com.br>
2023-03-02 17:25:56 -0300

    Animação, dados de uma api de filmes

---
+++ Aula10/js/anima1.js
@@ -0,0 +1,66 @@
+// function dizOi() {
+//   console.log("Oi");
+// }
+// window.setInterval(dizOi, 2000);
+
+// window.setInterval(() => {
+//   console.log("Oi");
+// }, 2000);
+
+// let x = 1;
+// window.setInterval(() => {
+//   console.log(`Contei ${x}`);
+//   x++;
+// }, 1000);
+
+const quadro = document.querySelector("#quadro");
+// let x = 50;
+// let r = 10;
+// window.setInterval(() => {
+//   //alterar os estilos de largura e altura do quadro
+//   x++;
+//   quadro.style.width = `${x}px`;
+//   quadro.style.height = `${x}px`;
+//   quadro.style.transform = `rotate(${r}deg)`;
+//   quadro.style.borderRadius = `${r / 2}px`;
+//   r++;
+// }, 100);
+
+// let pY = -500;
+
+// window.setInterval(() => {
+//   console.log(quadro.style.top);
+//   quadro.style.top = `${pY}px`;
+//   if (pY >= 10) {
+//     pY = 10;
+//   }
+//   pY += 2;
+// }, 20);
+
+// let t = -500;
+// const quadroA = window.setInterval(() => {
+//   console.log(quadro.style.top);
+//   quadro.style.top = `${t}px`;
+//   if (t >= 10) {
+//     window.clearInterval(quadroA);
+//   }
+//   t += 2;
+// }, 20);
+
+// let pX = 0;
+// let pY = 0;
+
+document.body.onclick = (event) => {
+  quadro.style.top = `${event.pageY}px`;
+  quadro.style.left = `${event.pageX}px`;
+};
+
+// function relogio() {
+//   var tempo = new Date();
+//   var hora = tempo.getHours();
+//   var minuto = tempo.getMinutes();
+//   var segundo = tempo.getSeconds();
+
+//   quadro.innerHTML = `${hora}:${minuto}:${segundo}`;
+// }
+// window.setInterval(relogio, 1000);
---
+++ Aula10/js/anima2.js
@@ -0,0 +1,26 @@
+function abrirJanela(dado) {
+  fundo.style.backgroundColor = "rgba(0,0,0,0.5)";
+  tela.style.top = "0px";
+  tela.innerHTML = `O valor de i é ${dado}`;
+}
+for (var i = 0; i < 10; i++) {
+  var btn = document.createElement("button");
+  btn.setAttribute("class", `btn`);
+  btn.innerHTML = `Botao ${i}`;
+  btn.onclick = () => abrirJanela(btn.outerText);
+  document.getElementById("botao").appendChild(btn);
+}
+
+// abrir.onclick = () => {
+//   fundo.style.backgroundColor = "rgba(0,0,0,0.5)";
+//   tela.style.top = "0px";
+//   console.log("Tela");
+// };
+
+fechar.onclick = () => {
+  console.log("fechar");
+  fundo.style.backgroundColor = "rgba(0,0,0,0)";
+  tela.style.top = "-600px";
+};
+
+// console.log(window.screen.width);
